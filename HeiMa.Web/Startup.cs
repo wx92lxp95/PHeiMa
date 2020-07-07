53baf4656182264c480c6491bbc490531fe8ba03
wangxin <541000714@qq.com>
2020-07-07 18:21:00 +0800

    推送测试

--- HeiMa.Web/Startup.cs
+++ HeiMa.Web/Startup.cs
@@ -16,7 +16,7 @@ namespace HeiMa.Web
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
